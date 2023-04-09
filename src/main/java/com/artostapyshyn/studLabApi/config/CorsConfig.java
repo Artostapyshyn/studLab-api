b9967df3775a0a661e3c9eee4ce07c544d9d0920
Artostapyshyn <artostapyshyn@gmail.com>
2023-04-09 14:02:11 +0300

    Added cors config.

---
+++ src/main/java/com/artostapyshyn/studLabApi/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.artostapyshyn.studLabApi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
