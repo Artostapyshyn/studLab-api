d617c9b4078c3740435a5374fe2b7da94b31cd7b
Artostapyshyn <artostapyshyn@gmail.com>
2023-10-29 22:19:08 +0200

    SocketJs updates.

--- src/main/java/com/artostapyshyn/studlabapi/config/WebSocketConfig.java
+++ src/main/java/com/artostapyshyn/studlabapi/config/WebSocketConfig.java
@@ -17,7 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
 }
