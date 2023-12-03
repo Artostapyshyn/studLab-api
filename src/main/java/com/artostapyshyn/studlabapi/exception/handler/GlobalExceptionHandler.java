00f2f464bc1d81ed7b6055f2ebab70b1b234fbae
Artostapyshyn <artostapyshyn@gmail.com>
2023-12-03 18:52:39 +0200

    Exception handling, removed vacancy and saved vacancy entities

--- src/main/java/com/artostapyshyn/studlabapi/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/artostapyshyn/studlabapi/exception/handler/GlobalExceptionHandler.java
@@ -2,10 +2,14 @@ package com.artostapyshyn.studlabapi.exception.handler;
 
 import io.jsonwebtoken.MalformedJwtException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.nio.file.AccessDeniedException;
 import java.security.SignatureException;
 
 @Log4j2
@@ -14,25 +18,43 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<Object> handleNullPointerException(NullPointerException ex) {
-        if (ex.getMessage() != null && ex.getMessage().contains("org.springframework.security.core.Authentication.getName()")) {
-            return ResponseEntity.internalServerError().body("Authentication error");
-        }
-        throw ex;
+        log.error("NullPointer exception", ex);
+        return ResponseEntity.internalServerError().body("Authentication error");
     }
 
     @ExceptionHandler(MalformedJwtException.class)
     public ResponseEntity<Object> handleMalformedJwtException(MalformedJwtException ex) {
-        if (ex.getMessage() != null && ex.getMessage().contains("io.jsonwebtoken.MalformedJwtException: JWT strings must contain exactly 2 period characters. Found: 0")) {
-            return ResponseEntity.internalServerError().body("JWT error");
-        }
-        throw ex;
+        log.error("JWT error", ex);
+        return ResponseEntity.internalServerError().body("JWT error");
     }
 
     @ExceptionHandler(SignatureException.class)
-    public ResponseEntity<Object> handleSignatureException(SignatureException ex) throws SignatureException {
-        if (ex.getMessage() != null && ex.getMessage().contains("io.jsonwebtoken.security.SignatureException: JWT signature does not match locally computed signature. JWT validity cannot be asserted and should not be trusted.")) {
-            return ResponseEntity.internalServerError().body("JWT expired and used again");
-        }
-        throw ex;
+    public ResponseEntity<Object> handleSignatureException(SignatureException ex) {
+        log.error("JWT expired and used again", ex);
+        return ResponseEntity.internalServerError().body("JWT expired and used again");
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<Object> handleDataAccessException(DataAccessException ex) {
+        log.error("Data access error", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Data access error");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException ex) {
+        log.error("Access denied error", ex);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied error");
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        log.error("No handler found for this request", ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No handler found for this request");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex) {
+        log.error("An unexpected error occurred", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
     }
 }
